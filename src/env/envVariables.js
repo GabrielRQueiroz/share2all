abb7ee1b4674a387d136f12e24fb5ceb0dcf4520
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2022-01-15 23:45:21 -0300

    Yet another build fix

---
+++ src/env/envVariables.js
@@ -0,0 +1,3 @@
+export const googleApi = process.env.REACT_APP_GOOGLE_TOKEN_API;
+export const sanityId = process.env.REACT_APP_SANITY_PROJECT_ID;
+export const sanityApi = process.env.REACT_APP_SANITY_TOKEN_API;
