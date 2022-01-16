c820c5e4831853a7e5c66a88d914a742cfd8f3fa
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2022-01-15 23:51:54 -0300

    Added environment exports

--- src/env/envVariables.js
+++ src/env/envVariables.js
@@ -1,3 +1,4 @@
 export const googleApi = process.env.REACT_APP_GOOGLE_TOKEN_API;
 export const sanityId = process.env.REACT_APP_SANITY_PROJECT_ID;
 export const sanityApi = process.env.REACT_APP_SANITY_TOKEN_API;
+
